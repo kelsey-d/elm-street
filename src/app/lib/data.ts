44253465e1e706b1c409b9460d4abb40b97c11f0
Kelsey <kelseydamier@outlook.com>
2025-11-07 09:02:52 -0500

    add header

---
+++ src/app/lib/data.ts
@@ -0,0 +1,15 @@
+const houses = [
+    {
+
+        image: '/starrett-mansion.jpg',
+        location: 'Port Townsend, WA'
+    },
+    {
+        image: '/sk-pierce.png',
+        location: 'Gardner, MA'
+    },
+    {
+        image: '/fictional.png',
+        location: 'Salem, MA'
+    },
+]
---
+++ tailwind.config.ts
@@ -0,0 +1,25 @@
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'sofia': ['var(--font-sofia-sans)'],
+        'sofia-condensed': ['var(--font-sofia-sans-extra-condensed)'],
+        'romanesco': ['var(--font-romanesco)'],
+      },
+      colors: {
+        'crimson': '#FF5500',
+        'ice': '#9EBFE3',
+      },
+    },
+  },
+  plugins: [],
+}
+
+export default config
